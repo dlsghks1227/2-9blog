237349a6fa868da62481b8a39a07b783c10d55e0
BJH <bqwert36@hanmail.net>
2020-11-19 01:53:44 +0900

    give header button state Data to child component

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from '../Header/Header';
 import Nav from "../Nav/Nav";
 import LandingContainer from '../LandingContainer/LandingContainer';
 import Footer from '../Footer/Footer';
 
 function LandingPage() {
+
+    const [isClicked, ClickOrNot] = useState(false);
     const style = {
         height: "100%"
     }
+    const navBtnClicked = ()=>{
+        ClickOrNot(!isClicked);
+    }
+
     return (
         <div className="landing-page" style={style}>
-            <Header />
-            <Nav />
+            <Header onClicked={navBtnClicked}/>
+            <Nav navState={isClicked}/>
             <LandingContainer />
             <Footer />
         </div>
