902ddf6e90ed009f62f05aff9fabd7d736339318
BJH <bqwert36@hanmail.net>
2020-11-18 17:08:36 +0900

    테스트를 위한 로그인 폼 작성

---
+++ client/src/components/views/login/login.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import './login.scss';
+
+function loginScreen() {
+
+    const login_Clicked = () => {
+        alert("login");
+    }
+
+    return (
+        <div className="login_part">
+            <h1>login</h1>
+            <input type="text" placeholder="아이디를 입력해주세요"></input><br></br>
+            <input type="password" placeholder="패스워드를 입력해주세요"></input><br></br><br></br>
+            <div className="login_btn">
+                <button onClick={login_Clicked}>
+                    login</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default loginScreen
