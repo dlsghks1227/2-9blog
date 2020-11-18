6d604d8d7d498851997612303178b19183610b21
BJH <bqwert36@hanmail.net>
2020-11-19 01:54:30 +0900

    add onclick arribute

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Header() {
+function Header(props) {
     return (
         <div className="header-inner">
-            <FontAwesomeIcon icon={faBars} className="bars" />
+            <FontAwesomeIcon icon={faBars} className="bars" onClick={props.onClicked} />
             <header id="header">
                 <NavLink className="blog-link" exact to="/"><p>BLOG</p></NavLink>
             </header>
