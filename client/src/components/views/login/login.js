764f42e90cf903f3e426035bdcfae7a55dde3bbc
BJH <bqwert36@hanmail.net>
2020-12-15 04:21:14 +0900

    로그인 페이지 수정 및 css 구현

--- client/src/components/views/login/login.js
+++ client/src/components/views/login/login.js
@@ -4,19 +4,23 @@ import './login.scss';
 function loginScreen() {
 
     const login_Clicked = () => {
+
     }
 
     return (
-        <div className="login_part">
-            <h1>login</h1>
-            <input type="text" placeholder="아이디를 입력해주세요"></input><br></br>
-            <input type="password" placeholder="패스워드를 입력해주세요"></input><br></br><br></br>
-            <div className="login_btn">
-                <button onClick={login_Clicked}>
-                    login</button>
+        <div className="LoginPage">
+            <div className="LoginPart">
+                <h1 className="LoginTitle">Welcome</h1>
+                <div className="LoginInput">
+                    <input type="text" size="30" placeholder="아이디를 입력해주세요"></input><br></br>
+                    <input type="password" size="30" placeholder="패스워드를 입력해주세요"></input><br></br><br></br>
+                </div>
+                <div className="LoginButton">
+                    <button onClick={login_Clicked}>로그인</button><br></br>
+                    <button onClick={login_Clicked}>회원가입</button>
+                </div>
             </div>
         </div>
-
     )
 }
 
