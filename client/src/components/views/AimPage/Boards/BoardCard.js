fe9ff3240d66c752c7b33c0545d6db456b2f232e
BJH <bqwert36@hanmail.net>
2020-12-21 01:51:23 +0900

    보드 삭제 기능 구현

--- client/src/components/views/AimPage/Boards/BoardCard.js
+++ client/src/components/views/AimPage/Boards/BoardCard.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React,{useRef} from 'react';
 //import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import boards from 'store/reducer/boards';
 import './BoardCardStyle.scss';
 import '../pages/BoardStyle.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { deleteBoard } from '../../../../store/reducer/boards';
+import { useDispatch } from 'react-redux';
 
 function BoardCard({ board }) {
-    return(
-    <div className="allBoard">
-        <div className="LinkStyle">
-            <Link to={`/board/${board.id}`}>{board.title}</Link>
+
+    const dispatch = useDispatch();
+
+    const delete_button = () => {
+        const action = deleteBoard(board.id);
+        dispatch(action);    
+    }
+
+    return (
+        <div className="allBoard">
+            <div className="LinkStyle">
+                <FontAwesomeIcon icon={faMinus} onClick={() => delete_button()} />
+                <Link to={`/board/${board.id}`}>{board.title}</Link>
+            </div>
         </div>
-    </div>)
+    );
 }
 
 export default BoardCard;
