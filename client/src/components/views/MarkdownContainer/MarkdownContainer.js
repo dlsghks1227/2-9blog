3d702dff81016ecbee71791bff3c9f38f065c0c8
Banana <lh1227@yahoo.co.kr>
2020-12-29 15:12:20 +0900

    게시글 수정 및 삭제 기능 구현

--- client/src/components/views/MarkdownContainer/MarkdownContainer.js
+++ client/src/components/views/MarkdownContainer/MarkdownContainer.js
@@ -5,27 +5,35 @@ import {
     useHistory,
 } from 'react-router-dom'
 import {
-    useSelector,
     useDispatch,
 } from 'react-redux';
 import MEDitor from "@uiw/react-md-editor";
 import './MarkdownContainer.scss'
 import {
     createPost,
+    updatePost,
+    deletePost
 } from '../../../store/reducer/api';
 import {
-    Button
+    Button,
+    Modal,
+    FormControl
 } from 'react-bootstrap'
 
-function MarkdownContainer() {
+function MarkdownContainer({ defaultTitle, defaultbody, postId }) {
     const history = useHistory();
 
     // 제목
-    const [title, setTitle] = useState('');
+    const [title, setTitle] = useState(defaultTitle ? defaultTitle : '');
     // 본문
-    const [body, setBody] = useState(``);
+    const [body, setBody] = useState(defaultbody ? defaultbody : '');
+
+    const [isShowDelete, setIsShowDeleteMdal] = useState(false);
+
     const dispatch = useDispatch();
     const onSubmit = (postData) => dispatch(createPost(postData));
+    const onUpdate = (postData) => dispatch(updatePost(postData));
+    const onDelete = (postData) => dispatch(deletePost(postData));
 
     const onSubmitClick = async (event) => {
         event.preventDefault();
@@ -39,20 +47,59 @@ function MarkdownContainer() {
         }
     }
 
-    const onTitleChange = (event) => {
-        setTitle(event.target.value)
+    const onModifiedClick = async (event) => {
+        event.preventDefault();
+        const data = await onUpdate({
+            title: title,
+            body: body,
+            id: postId
+        });
+        if (data.message !== "fail") {
+            history.push(`/post/read/${data.id}`)
+        }
+    }
+
+    const onDeleteClick = async (event) => {
+        event.preventDefault();
+        const data = await onDelete({
+            id: postId
+        });
+        if (data.message !== "fail") {
+            history.push(`/post`)
+        }
     }
 
     return (
         <div className="container">
-            <input value={title} onChange={(e) => setTitle(e.target.value)} className="title-input" placeholder="제목"/>
+            <Modal
+                show={isShowDelete}
+                onHide={() => setIsShowDeleteMdal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>삭제하시겠습니까?</Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                    <Button variant="outline-danger" onClick={onDeleteClick}>삭제</Button>
+                    <Button variant="outline-primary" onClick={() => setIsShowDeleteMdal(false)}>취소</Button>
+                </Modal.Footer>
+            </Modal>
+            <input value={title} onChange={(e) => setTitle(e.target.value)} className="title-input" placeholder="제목" />
 
-            <MEDitor height={600} value={body} onChange={setBody}/>
-            
-            <Button className="md-submit-button" size="lg" onClick={onSubmitClick}>Write</Button>
-            {/* <MEDitor.Markdown source={value} /> */}
+            <MEDitor height={600} value={body} onChange={setBody} />
+
+            {
+                postId ?
+                    (
+                        <div>
+                            <Button variant="outline-primary" className="md-submit-button" size="lg" onClick={onModifiedClick}>수정</Button>
+                            <Button variant="outline-danger" className="md-submit-button mr-3" size="lg" onClick={() => setIsShowDeleteMdal(true)}>삭제</Button>
+                        </div>
+                    ) :
+                    (
+                        <Button variant="outline-primary" className="md-submit-button" size="lg" onClick={onSubmitClick}>Write</Button>
+                    )
+            }
         </div>
     );
 }
 
-export default MarkdownContainer;  
+export default MarkdownContainer;
